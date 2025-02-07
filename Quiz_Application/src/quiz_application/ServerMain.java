939298ce45fddbcbe52674f346899ac7317d74ab
KrasimirKarabulev <116200275+KrasimirKarabulev@users.noreply.github.com>
2025-02-08 00:52:13 +0200

    Add files via upload

---
+++ Quiz_Application/src/quiz_application/ServerMain.java
@@ -0,0 +1,11 @@
+package quiz_application;
+public class ServerMain {
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.start();
+		
+
+	}
+
+}
---
+++ Quiz_Application/src/quiz_application/Student.java
@@ -0,0 +1,40 @@
+package quiz_application;
+import java.io.Serializable;
+import java.util.regex.Pattern;
+
+public class Student implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final Pattern EGNPattern = Pattern.compile("d\\{10}");
+	private static final Pattern facultyNumberPattern = Pattern.compile("d\\{9}");
+	private String EGN;
+	private String facultyNumber;
+
+	public Student(String EGN, String facultyNumber) throws StudentException {
+		if (!EGNPattern.matcher(EGN).matches() || !facultyNumberPattern.matcher(facultyNumber).matches()) {
+			throw new StudentException("Invalid format");
+		}
+		this.EGN = EGN;
+		this.facultyNumber = facultyNumber;
+	}
+
+	public String getEGN() {
+		return EGN;
+	}
+
+	public void setEGN(String eGN) {
+		EGN = eGN;
+	}
+
+	public String getFacultyNumber() {
+		return facultyNumber;
+	}
+
+	public void setFacultyNumber(String facultyNumber) {
+		this.facultyNumber = facultyNumber;
+	}
+
+}
